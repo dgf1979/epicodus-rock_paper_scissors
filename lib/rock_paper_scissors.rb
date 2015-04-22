6e91f5d7a5b5b2da214fbd0c37cf1534910d3ae1
Drew & Paul <student@epicodus.com>
2015-04-22 15:25:56 -0700

    stubbed in logic for remaining true-false cases (refactor)

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -3,9 +3,29 @@ class String
     player = self
     game_result = ""
 
+    # possible combos
+    #
+    # player == opponent (tie)
+    #
+    # rock beats scissors (true)
+    # scissors beats paper (true)
+    # paper beats rock (true)
+    #
+    # rock beat paper (false)
+    # scissors beats rock (false)
+    # paper beat scissors (false)
+
     if player == "rock" && opponent == "scissors"
       game_result = "true"
+    elsif player == "scissors" && opponent == "paper"
+        game_result = "true"
+    elsif player == "paper" && opponent == "rock"
+      game_result = "true"
+    elsif player == "rock" && opponent == "paper"
+      game_result = "false"
     elsif player == "scissors" && opponent == "rock"
+      game_result = "false"
+    elsif player == "paper" && opponent == "scissors"
       game_result = "false"
     end
 
