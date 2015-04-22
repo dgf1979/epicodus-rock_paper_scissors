4d60b333022d676951f9a84914c20656f46f7ffe
Drew & Paul <student@epicodus.com>
2015-04-22 15:15:30 -0700

    added logic to pass second unit test

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -5,6 +5,8 @@ class String
 
     if player == "rock" && opponent == "scissors"
       game_result = "true"
+    elsif player == "scissors" && opponent == "rock"
+      game_result = "false"
     end
 
     game_result
