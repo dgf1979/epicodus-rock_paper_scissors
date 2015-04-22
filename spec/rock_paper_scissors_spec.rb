a01adb26fb26d003bf9f4a71fbd15650f6d702f5
Drew & Paul <student@epicodus.com>
2015-04-22 16:16:02 -0700

    refactor unit tests to reflect change in desired output

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -4,19 +4,19 @@ require "rock_paper_scissors"
 
 describe("Rock, paper, scissors") do
   it("returns true if rock is the object and scissors is the argument") do
-    expect(("rock").beats("scissors")).to(eq("true"))
+    expect(("rock").beats("scissors")).to(eq("Rock Wins!"))
   end
 
   it("returns false if scissors is the object and rock is the argument") do
-    expect(("scissors").beats("rock")).to(eq("false"))
+    expect(("scissors").beats("rock")).to(eq("Scissors Wins!"))
   end
 
   it("returns 'tie' if object and argument are equal") do
-    expect(("scissors").beats("scissors")).to(eq("tie"))
+    expect(("scissors").beats("scissors")).to(eq("Tie!"))
   end
 
   it("functions if capitalized words or letters are input") do
-    expect(("SCISSORS").beats("scissors")).to(eq("tie"))
+    expect(("SCISSORS").beats("scissors")).to(eq("Tie!"))
   end
 
 end
